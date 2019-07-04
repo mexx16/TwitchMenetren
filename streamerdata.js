79dc049ad61014bd43acca11315586e7a1214ea0
mexx16 <mexx.designz@gmail.com>
2019-07-04 19:29:26 +0000

    Initial commit

---
+++ streamerdata.js
@@ -0,0 +1,14 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
+var streamer = "mexx_official";
+//Kép megjelenítése amikor üres a menetrend.
+var noEventsPic = "https://peldakep.hu/kep.png";
+//Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
+var offlinePic = "https://peldakep.hu/kep.png";
+//Offline állapotban megjelenő szöveg
+var offlineText = "Nincs internet!"; 
+//Üres menetrendkor megjelenő szöveg
+var noEventsText = "Menetrend Jelenleg üres!";
+
